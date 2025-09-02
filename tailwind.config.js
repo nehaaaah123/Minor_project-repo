1012c985c1e1098f9b3eb411e28dd6dba082d2b5
Neha <130530380+nehaaaah123@users.noreply.github.com>
2025-09-02 23:07:36 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend:{
+      fontFamily: {
+        gruppo: ['"Gruppo"', 'sans-serif'],
+        habibi: ['"Habibi"', 'serif'],
+        electrolize: ['"Electrolize"', 'sans-serif'],
+        fira: ['"Fira Sans"', 'sans-serif'],
+      },
+      colors:{
+        customColors:{
+          'lightPurple': '#8E5AD0',
+          'radialPurpleL': '#C8ACD6',
+          'radialPurpleD': '#694F8E',
+          'offWhite': '#FFF0D2',
+          'offWhiteDark':'#FFF2C3',
+          'fadedPurple': '#F1EAFF',
+          'lightGreen': '#DEF9C4',
+          'darkGreen': '#88D66C',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
